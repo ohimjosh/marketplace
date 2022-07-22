c5d23cd2f4e7b64b8e6cb0c38db907cefa946f88
Josh <joshmiranda.exe@gmail.com>
2022-07-22 13:59:35 -0400

    closer markers

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -148,8 +148,8 @@ const farOptions = {
 
 const generateHouses = (position: LatLngLiteral) => {
   const _houses: Array<LatLngLiteral> = [];
-  for (let i = 0; i < 100; i++) {
-    const direction = Math.random() < 0.5 ? -2 : 2;
+  for (let i = 0; i < 50; i++) {
+    const direction = Math.random() < 0.5 ? -35 : 35;
     _houses.push({
       lat: position.lat + Math.random() / direction,
       lng: position.lng + Math.random() / direction,
